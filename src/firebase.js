89b258a2582922362d44ba61df1ef886137fed01
Alex Markin <alexndr86@gmail.com>
2020-09-30 20:31:12 +0300

    Clear setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyApZDwwpewLCphVc18JdChwCsiVPH1z-I4",
+  authDomain: "clone-15b44.firebaseapp.com",
+  databaseURL: "https://clone-15b44.firebaseio.com",
+  projectId: "clone-15b44",
+  storageBucket: "clone-15b44.appspot.com",
+  messagingSenderId: "177679673209",
+  appId: "1:177679673209:web:30cc2aa338b73a0248f1a8",
+};
