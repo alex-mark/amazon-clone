4ce109b5e4115fb2eb387df024732ae8daeebbf7
Alex Markin <alexndr86@gmail.com>
2020-10-01 19:24:01 +0300

    Add styles to subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,33 @@
 import React from "react";
+import CurrencyFormat from "react-currency-format";
 import "./Subtotal.css";
 
 function Subtotal() {
-  return <div></div>;
+  const basket = ["sdf", "sdsdf"];
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items):
+              <strong>{` ${value}`}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
 
 export default Subtotal;
