750336ec74ee61a90f8272c7c7138461c9b616fb
Alex Markin <alexndr86@gmail.com>
2020-10-02 21:12:40 +0300

    Style payment component

--- src/Payment.js
+++ src/Payment.js
@@ -1,8 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import BasketItem from "./BasketItem";
 
 function Payment() {
-  return <div className="payment"></div>;
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Street</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item, index) => (
+              <BasketItem item={item} key={item.key} index={index} />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">{/* stripe magic */}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Payment;
