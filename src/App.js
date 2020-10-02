9241f35dffa14cc48fd480214d4eb56339cc0f91
Alex Markin <alexndr86@gmail.com>
2020-10-02 12:58:54 +0300

    Add login component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Login from "./Login";
 import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
           <Route path="/" exact>
+            <Header />
             <Home />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </div>
